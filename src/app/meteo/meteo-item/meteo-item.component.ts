ced5c94b156ecadda89daeaf69feb9ebd42f9899
Alexis Cabrol <alexiscabrololivaux@gmail.com>
2020-05-19 22:28:52 +0200

    Ajout style image

--- src/app/meteo/meteo-item/meteo-item.component.ts
+++ src/app/meteo/meteo-item/meteo-item.component.ts
@@ -15,5 +15,4 @@ export class MeteoItemComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/models/MeteoItem.ts
+++ src/app/models/MeteoItem.ts
@@ -4,4 +4,6 @@ export class MeteoItem extends JSONBuilder {
 
   public static readonly CONST_VILLE_PAR_DEFAULT = 'Lyon';
   name: string;
+  main: any;
+  weather: any;
 }
